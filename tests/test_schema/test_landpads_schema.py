a852c696af5361cc402c5f6672be21c6ab1009dc
Justin Yamamoto <99459474+JustinYSJSU@users.noreply.github.com>
2025-12-27 21:29:59 -0800

    Update test_landpads_schema.py

--- tests/test_schema/test_landpads_schema.py
+++ tests/test_schema/test_landpads_schema.py
@@ -39,11 +39,11 @@ class TestSchemaLandpads:
             valid_schema = schema_data("single_landpad.yaml")
         self.verify_schema_landpad(valid_schema, url)
 
-    def verify_schema_landpad(self, valid_schama, url):
+    def verify_schema_landpad(self, valid_schema, url):
         response = requests.get(url)
 
         assert response.status_code == 200
         try: 
-            validate(response.json(), valid_schama)
+            validate(response.json(), valid_schema)
         except ValidationError as e:
             pytest.fail(f"Failed to validate schema: {e.message}")
