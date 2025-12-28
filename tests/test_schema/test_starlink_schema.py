22b451f5cf1dcc7facd4f6302d93665dc8e9eceb
Justin Yamamoto <99459474+JustinYSJSU@users.noreply.github.com>
2025-12-27 21:31:57 -0800

    Update test_starlink_schema.py

--- tests/test_schema/test_starlink_schema.py
+++ tests/test_schema/test_starlink_schema.py
@@ -39,10 +39,10 @@ class TestSchemaStarlink:
             valid_schema = schema_data("single_satellite.yaml")
         self.verify_schema_starlink(valid_schema, url)
 
-    def verify_schema_starlink(self, valid_schama, url):
+    def verify_schema_starlink(self, valid_schema, url):
         response = requests.get(url)
 
         assert response.status_code == 200
-        try: validate(response.json(), valid_schama)
+        try: validate(response.json(), valid_schema)
         except ValidationError as e:
              pytest.fail(f"Failed to validate schema: {e.message}")
