410d279ca486d189f54a0310a420a9763c37f559
Justin Yamamoto <99459474+JustinYSJSU@users.noreply.github.com>
2025-12-27 21:31:44 -0800

    Update test_ships_schema.py

--- tests/test_schema/test_ships_schema.py
+++ tests/test_schema/test_ships_schema.py
@@ -39,10 +39,10 @@ class TestSchemaRockets:
             valid_schema = schema_data("single_ship.yaml")
         self.verify_schema_ships(valid_schema, url)
 
-    def verify_schema_ships(self, valid_schama, url):
+    def verify_schema_ships(self, valid_schema, url):
         response = requests.get(url)
 
         assert response.status_code == 200
-        try: validate(response.json(), valid_schama)
+        try: validate(response.json(), valid_schema)
         except ValidationError as e:
              pytest.fail(f"Failed to validate schema: {e.message}")
